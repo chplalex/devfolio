84f290c8ad53588f70e59ba92460a78bf1b0dc86
chplALEX <chplalex.dev@gmail.com>
2024-08-08 12:57:03 +0300

    Upgraded: - Flutter to 3.24.0 - Gradle Plugin to 8.4.2 - Kotlin to 1.9.23 - Android target SDK to 34 - iOS minimum to 12.0 - Java to 17 - Flutter and android dependencies Removed unused dependencies. Resolved conflicts amd deprecations. Fixed analyzer remarks. Runnable on Android, iOS and Web

--- lib/configs/core_theme.dart
+++ lib/configs/core_theme.dart
@@ -9,12 +9,12 @@ final themeLight = ThemeData(
   highlightColor: Colors.black,
   canvasColor: Colors.white,
   fontFamily: fontFamily,
-  backgroundColor: Colors.white,
   splashColor: Colors.transparent,
   scaffoldBackgroundColor: Colors.white,
   colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red).copyWith(
     secondary: Colors.black,
     brightness: Brightness.light,
+    surface: Colors.white,
   ),
 );
 
@@ -26,10 +26,10 @@ final themeDark = ThemeData(
   canvasColor: Colors.white,
   fontFamily: fontFamily,
   splashColor: Colors.transparent,
-  backgroundColor: Colors.grey[800],
   scaffoldBackgroundColor: Colors.black,
   colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red).copyWith(
     secondary: const Color(0xffC0392B),
     brightness: Brightness.dark,
+    surface: Colors.grey[800],
   ),
 );
